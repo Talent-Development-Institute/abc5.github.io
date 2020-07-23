d16fe17a7add1a30aaf9fdca0ca40dda286d6a30
RomanYuen <jasonyuen1001@gmail.com>
2020-07-23 18:56:15 +0800

    index renew

---
+++ js/navbarMobileController.js
@@ -0,0 +1,4 @@
+function navbarBtnCheck() {
+  document.getElementById("navbarMB").setAttribute("aria-expanded", "false");
+  document.getElementById("navbarSupportedContent").classList.remove("show");
+}
---
+++ js/signUpForOnlineMeeting.js
@@ -0,0 +1,5 @@
+function signUPMeeting() {
+  window.open(
+    "https://us02web.zoom.us/meeting/register/tZAtcO-spzMoE9eyDahwQKdgHSrQB0w-sjCR"
+  );
+}
