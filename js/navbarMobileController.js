e578d6dffcdeaac8895b4b7bfe6f3f6237457401
Gabriel Chan <gabriel@thinkpanda.com>
2020-11-24 17:09:40 +0800

    Production BETA

--- js/navbarMobileController.js
+++
@@ -1,4 +0,0 @@
-function navbarBtnCheck() {
-  document.getElementById("navbarMB").setAttribute("aria-expanded", "false");
-  document.getElementById("navbarSupportedContent").classList.remove("show");
-}
--- js/signUpForOnlineMeeting.js
+++
@@ -1,3 +0,0 @@
-function signUPMeeting() {
-  window.open("https://bit.ly/3g4FnOW");
-}
