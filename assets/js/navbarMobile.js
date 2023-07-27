18867f202e71de040e088ef96ba89762346a0929
lokyi <105396633+louiselokyi@users.noreply.github.com>
2023-07-27 17:33:21 +0800

    Delete assets directory

--- assets/js/navbarMobile.js
+++
@@ -1,8 +0,0 @@
-
-var menuTrigger = document.getElementById("menuTrigger");
-var navMB = document.getElementById("navMB");
-
-function myFunction1() {
-    menuTrigger.classList.remove("active");
-    navMB.style.display = "none";
-}
